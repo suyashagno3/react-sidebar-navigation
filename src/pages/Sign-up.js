c1b0d45b1882f1f47f34f60fb6ae3ecb88abe327
Suyash Agnihotri <suyash1304@gmail.com>
2020-12-22 23:22:17 +0530

    add

---
+++ src/pages/Sign-up.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Signup() {
+    return (
+        <div className='signup'>
+            <h1>Sign-Up</h1>
+        </div>
+        
+    )
+}
+
+export default Signup
